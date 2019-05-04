45587a54fcaeb62c0ce8a8b01f6cc2d33f161ef5
Oscar Shoudt <shoudto@gmail.com>
2019-05-04 16:54:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,15 +11,4 @@ class Song
   def save
     self.class.all << self
   end
-  
-  def self.find_by_name(name)
-   @all.find do |song|
-      song.name == name
-    end
-  end 
- 
-  # end of code 
-  def self.destroy_all
-    self.all.clear 
-  end 
 end
