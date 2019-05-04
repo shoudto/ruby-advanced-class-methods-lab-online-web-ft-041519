0953bc4ffc2abffb98aba9c39340a5f1bd97d4eb
Oscar Shoudt <shoudto@gmail.com>
2019-05-04 17:52:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
   def self.new_from_filename(filename)
     binding.pry
     filename = filename.split(" - ")
-    song_name = filename[1].delete(".mp3")
+    song_name = filename.gsub(".mp3")
     
   end 
   
